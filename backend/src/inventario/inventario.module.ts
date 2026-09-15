db369ddbb1624e31d981116c418c0ac504d708e1
jose27luis <18121040@unamad.edu.pe>
2026-09-15 12:43:13 -0500

    feat(backend): registrar el módulo de inventario

---
+++ backend/src/inventario/inventario.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { InventarioController } from './inventario.controller';
+import { InventarioService } from './inventario.service';
+import { MovimientosService } from './movimientos.service';
+
+@Module({
+  controllers: [InventarioController],
+  providers: [InventarioService, MovimientosService],
+  exports: [InventarioService, MovimientosService],
+})
+export class InventarioModule {}
