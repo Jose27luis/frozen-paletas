2690472d31711c4a4c49b6f281ecc70b38f7d9e5
jose27luis <18121040@unamad.edu.pe>
2026-09-16 18:18:30 -0500

    feat(movil): montar los modulos solo cuando se visitan

--- movil/lib/funciones/marco.dart
+++ movil/lib/funciones/marco.dart
@@ -4,26 +4,49 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../nucleo/modulos.dart';
 import '../nucleo/proveedores.dart';
 import 'inventario.dart';
+import 'lotes.dart';
 import 'mermas.dart';
 import 'panel.dart';
 import 'produccion.dart';
+import 'sabores.dart';
 import 'salidas.dart';
+import 'usuarios.dart';
 
-class Marco extends ConsumerWidget {
+const Map<Modulo, Widget> _pantallas = <Modulo, Widget>{
+  Modulo.panel: PantallaPanel(),
+  Modulo.inventario: PantallaInventario(),
+  Modulo.produccion: PantallaProduccion(),
+  Modulo.salidas: PantallaSalidas(),
+  Modulo.mermas: PantallaMermas(),
+  Modulo.lotes: PantallaLotes(),
+  Modulo.sabores: PantallaSabores(),
+  Modulo.usuarios: PantallaUsuarios(),
+};
+
+class Marco extends ConsumerStatefulWidget {
   const Marco({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<Marco> createState() => _MarcoState();
+}
+
+class _MarcoState extends ConsumerState<Marco> {
+  final Set<Modulo> _visitados = <Modulo>{Modulo.panel};
+
+  @override
+  Widget build(BuildContext context) {
     final Modulo modulo = ref.watch(moduloProvider);
+
+    _visitados.add(modulo);
 
     return IndexedStack(
       index: modulo.index,
-      children: const <Widget>[
-        PantallaPanel(),
-        PantallaInventario(),
-        PantallaProduccion(),
-        PantallaSalidas(),
-        PantallaMermas(),
+      children: <Widget>[
+        for (final Modulo cada in Modulo.values)
+          if (_visitados.contains(cada))
+            _pantallas[cada]!
+          else
+            const SizedBox.shrink(),
       ],
     );
   }
