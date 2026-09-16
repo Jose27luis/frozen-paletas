459f48a985d57a593e8e69a4c5605ac5c72babb5
jose27luis <18121040@unamad.edu.pe>
2026-09-16 17:56:51 -0500

    feat(movil): abrir el menu lateral desde mermas

--- movil/lib/funciones/mermas.dart
+++ movil/lib/funciones/mermas.dart
@@ -7,6 +7,7 @@ import '../dominio/modelos.dart';
 import '../nucleo/formato.dart';
 import '../nucleo/proveedores.dart';
 import '../nucleo/tema.dart';
+import '../ui/menu_lateral.dart';
 import '../ui/piezas.dart';
 
 class PantallaMermas extends ConsumerWidget {
@@ -19,6 +20,7 @@ class PantallaMermas extends ConsumerWidget {
     final bool puede = usuario?.puede(Permisos.registrarMermas) ?? false;
 
     return Scaffold(
+      drawer: const MenuLateral(),
       appBar: AppBar(title: const Text('Mermas')),
       floatingActionButton: puede
           ? FloatingActionButton.extended(
