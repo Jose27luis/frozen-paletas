68046df997180100b4125d8d8ef0b073417cacdb
jose27luis <18121040@unamad.edu.pe>
2026-09-16 16:54:22 -0500

    feat(movil): adjuntar el token a cada petición

---
+++ movil/lib/datos/cliente_api.dart
@@ -0,0 +1,82 @@
+import 'package:dio/dio.dart';
+
+import '../nucleo/entorno.dart';
+import 'almacen_sesion.dart';
+
+class ClienteApi {
+  ClienteApi(this._almacen)
+      : _dio = Dio(
+          BaseOptions(
+            baseUrl: Entorno.api,
+            connectTimeout: Entorno.esperaMaxima,
+            receiveTimeout: Entorno.esperaMaxima,
+            headers: <String, String>{'Accept': 'application/json'},
+          ),
+        ) {
+    _dio.interceptors.add(InterceptorsWrapper(onRequest: _adjuntarToken));
+  }
+
+  final Dio _dio;
+  final AlmacenSesion _almacen;
+
+  Future<List<Map<String, Object?>>> lista(
+    String ruta, {
+    Map<String, Object?>? parametros,
+  }) async {
+    final Response<List<Object?>> respuesta = await _dio.get<List<Object?>>(
+      ruta,
+      queryParameters: _limpiar(parametros),
+    );
+
+    return (respuesta.data ?? <Object?>[])
+        .map((Object? fila) => fila! as Map<String, Object?>)
+        .toList(growable: false);
+  }
+
+  Future<Map<String, Object?>> objeto(
+    String ruta, {
+    Map<String, Object?>? parametros,
+  }) async {
+    final Response<Map<String, Object?>> respuesta =
+        await _dio.get<Map<String, Object?>>(
+      ruta,
+      queryParameters: _limpiar(parametros),
+    );
+
+    return respuesta.data ?? <String, Object?>{};
+  }
+
+  Future<Map<String, Object?>> enviar(
+    String ruta,
+    Map<String, Object?> cuerpo,
+  ) async {
+    final Response<Map<String, Object?>> respuesta =
+        await _dio.post<Map<String, Object?>>(ruta, data: cuerpo);
+
+    return respuesta.data ?? <String, Object?>{};
+  }
+
+  Map<String, Object?>? _limpiar(Map<String, Object?>? parametros) {
+    if (parametros == null) {
+      return null;
+    }
+
+    return <String, Object?>{
+      for (final MapEntry<String, Object?> dato in parametros.entries)
+        if (dato.value != null && dato.value != '') dato.key: dato.value,
+    };
+  }
+
+  Future<void> _adjuntarToken(
+    RequestOptions opciones,
+    RequestInterceptorHandler siguiente,
+  ) async {
+    final String? token = await _almacen.leerToken();
+
+    if (token != null && token.isNotEmpty) {
+      opciones.headers['Authorization'] = 'Bearer $token';
+    }
+
+    siguiente.next(opciones);
+  }
+}
