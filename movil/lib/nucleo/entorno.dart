83b4b69f0c49f9721e885bf6e156f4ba013e756f
jose27luis <18121040@unamad.edu.pe>
2026-09-16 16:54:22 -0500

    feat(movil): apuntar la app al dominio de producción

---
+++ movil/lib/nucleo/entorno.dart
@@ -0,0 +1,5 @@
+abstract final class Entorno {
+  static const String servidor = 'https://frozen.grupovalderrama.pe';
+  static const String api = '$servidor/api';
+  static const Duration esperaMaxima = Duration(seconds: 20);
+}
